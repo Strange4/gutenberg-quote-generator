c9a3576fac42610374ae63fdd186a7e7fa6040ac
Andrew T <turinity57@gmail.com>
2023-03-07 10:50:56 -0500

    Basic project structure and vscode settings

---
+++ QuoteUploader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuoteUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
