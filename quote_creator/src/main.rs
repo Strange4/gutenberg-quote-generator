94df108a08bf602631213b5b14581f1c91c657aa
Strange4 <>
2023-03-12 22:11:05 -0400

    add links spinner and default value

--- quote_creator/src/main.rs
+++ quote_creator/src/main.rs
@@ -3,10 +3,9 @@ mod gutendex;
 use futures::{stream, StreamExt};
 use gutendex::BookInfo;
 use reqwest::Client;
-use tokio::fs::File;
-use tokio::io::AsyncWriteExt;
+use std::{fs::File, time::Duration};
 use lazy_static::lazy_static;
-use std::io::Error;
+use std::io::{Error, Write};
 use regex::Regex;
 use std::sync::Arc;
 use indicatif::{ProgressBar, ProgressStyle};
@@ -14,19 +13,22 @@ use dialoguer::{theme::ColorfulTheme, Input};
 
 const PARALLEL_REQUESTS: usize = 10;
 const BOOKS_FOLDER: &str = "../books_scrapped";
+const BOOK_AMMOUNT: u32 = 100;
 
 #[tokio::main]
 async fn main() {
     let books_to_download = get_number_of_books();
     let folder = get_folder();
 
-    let progress = Arc::new(
-        create_progress_bar(books_to_download));
-        
+    
+    let spinner = create_spinner("Getting links".to_string());
     let books = tokio::task::spawn_blocking(
         move || gutendex::get_top_book_links(books_to_download))
         .await.unwrap();
-
+    spinner.finish();
+    println!("Getting the books...");
+    let progress = Arc::new(
+        create_progress_bar(books_to_download));
     let client = Client::new();
     let bodies = stream::iter(books)
         .map(|book| {
@@ -54,12 +56,13 @@ async fn main() {
 async fn get_book(book: BookInfo, client: Client, progress: Arc<ProgressBar>, save_folder: String) -> Option<File>{
     let response = client.get(&book.guten_url)
     .send().await.unwrap();
+    progress.inc(1);
     if response.status().is_success() {
         let text = response.text()
             .await.unwrap();
         let book_text = parse_book(text.as_str());
         let file = write_book_to_file(&book.title, book_text, save_folder)
-            .await.unwrap();
+            .unwrap();
         progress.inc(1);
         return Some(file);
     }
@@ -79,14 +82,14 @@ fn parse_book(text: &str) -> &str {
     book.trim()
 }
 
-async fn write_book_to_file(title: &String, book: &str, mut folder: String) -> Result<File, Error>{
+fn write_book_to_file(title: &String, book: &str, mut folder: String) -> Result<File, Error>{
     folder.push('/');
     folder.push_str(&clean_file_name(title));
     folder.push_str(".txt");
-    let mut file = File::create(folder).await?;
-    file.write_all(book.as_bytes()).await?;
+    let mut file = File::create(folder)?;
+    file.write_all(book.as_bytes())?;
     // why do i have to flush???
-    file.flush().await?;
+    // file.flush().await?;
     Ok(file)
 }
 
@@ -100,25 +103,49 @@ fn clean_file_name(title: &String) -> String{
 }
 
 fn create_progress_bar(books_to_download: u32) -> ProgressBar {
-    let progress = ProgressBar::new(books_to_download as u64);
+    let progress = ProgressBar::new(books_to_download as u64 * 2);
+    progress.enable_steady_tick(Duration::from_millis(120));
     progress.set_style(
         ProgressStyle::with_template(
             "{spinner:.blue} [{elapsed_precise}] [{bar:40.cyan/blue}] ({pos}/{len}, ETA {eta})"
         ).unwrap()
         .tick_strings(&[
-            "◐",
-			"◓",
-			"◑",
-			"◒",
+            "▹▹▹▹▹",
+            "▸▹▹▹▹",
+            "▹▸▹▹▹",
+            "▹▹▸▹▹",
+            "▹▹▹▸▹",
+            "▹▹▹▹▸",
             "▪▪▪▪▪",
         ])
     );
     progress
 }
+fn create_spinner(message: String) -> ProgressBar{
+    let pb = ProgressBar::new_spinner();
+    pb.enable_steady_tick(Duration::from_millis(100));
+    pb.set_style(
+        ProgressStyle::with_template("{spinner:.blue} {msg}")
+            .unwrap()
+            .tick_strings(&[
+                "◜",
+                "◠",
+                "◝",
+                "◞",
+                "◡",
+                "◟",
+                "▪▪▪▪▪",
+            ]),
+    );
+    pb.set_message(message);
+    pb
+}
+
 
 fn get_number_of_books() -> u32{
     Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Enter the number of books to download")
+        .default(BOOK_AMMOUNT)
         .interact_text()
         .unwrap()
 }
